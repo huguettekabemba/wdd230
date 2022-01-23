8056922be53571a5699397d0d184841fb6b785bd
huguettekabemba <kab21012@byui.edu>
2022-01-23 03:45:37 +0200

    commit

--- lesson2/js/date.js
+++ lesson2/js/date.js
@@ -1,4 +1,3 @@
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = `Last Updated: ${document.lastModified}`;
-
 document.querySelector('#currentyear').textContent = new Date(Date.now()).getFullYear();
