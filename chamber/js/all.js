8a611d85d6806f17ee2c282457b6bc3fb6f7f2fa
huguettekabemba <kab21012@byui.edu>
2022-01-30 20:02:38 +0200

    commit

---
+++ chamber/js/all.js
@@ -0,0 +1,44 @@
+
+// button responsive 
+const button = document.querySelector('#hamb');
+const firstNav = document.querySelector('.nav1');
+
+button.onclick = () => {
+    firstNav.classList.toggle('open');
+    button.classList.toggle('open');
+}
+
+// current year
+
+const date = new Date();
+
+let currentYear = date.getFullYear();
+let homeYear = document.querySelector('#year');
+
+homeYear.innerHTML = currentYear;
+
+// last modification
+
+let lastModification = document.lastModified;
+
+let lastTime = document.getElementById('lastmod');
+lastTime.innerHTML = lastModification;
+
+// current date with format: Wednesday, 24 July 2020.
+
+const datefield = document.getElementById('currenttime')
+
+const fulldate = new Intl.DateTimeFormat('en-Us',{
+    dateStyle: "full"
+}).format(date);
+
+datefield.innerHTML = fulldate;
+
+// remove responsive when the view is large
+
+window.onresize = () => {
+    if(window.innerWidth > 1000){
+    firstNav.classList.remove('open');
+    button.classList.remove('open')
+    }
+}
--- js/dates.js
+++ js/dates.js
@@ -1,4 +1,4 @@
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = `Last Updated: ${document.lastModified}`;
-
 document.querySelector('#currentyear').textContent = new Date(Date.now()).getFullYear();
+
