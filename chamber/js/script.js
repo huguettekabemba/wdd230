119e81cfa132cd81b62474296a0585e2c08b8889
huguettekabemba <kab21012@byui.edu>
2022-02-04 20:58:02 +0200

    comm

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,13 +1,3 @@
-
-// button toggle menu
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
-const x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
-
 // current year
 
 const date = new Date();
