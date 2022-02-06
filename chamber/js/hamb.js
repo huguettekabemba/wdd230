973b430c1560c31b5ac3064eafc9959007f8a6bf
huguettekabemba <kab21012@byui.edu>
2022-02-06 08:43:43 +0200

    done

--- chamber/js/hamb.js
+++ chamber/js/hamb.js
@@ -2,6 +2,5 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamb").classList.toggle("open");
 }
-
 const x = document.getElementById('hamb');
 x.onclick = toggleMenu;
