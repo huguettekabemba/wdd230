a523bd8088988836a60c6a52ba6667578a144617
huguettekabemba <kab21012@byui.edu>
2022-02-26 03:35:12 +0200

    commit

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -21,11 +21,6 @@ x.onclick = toggleMenu;
 
 
 
-//const fBase = document.querySelector(".fb");
-//const fMed = document.querySelector(".fm");
-//const fLarge = document.querySelector(".fl");
-
-
 
 //day names array
 const daynames = [
@@ -55,17 +50,15 @@ const months = [
 ];
 
 const today = new Date();
-
 const dayName = daynames[today.getDay()];
 const monthName = months[today.getMonth()];
 const dateNum = today.getDate();
 const year = today.getFullYear();
 
-document.querySelector(".date").innerHTML = `<p><strong>${dayName}, ${dateNum} ${monthName} ${year}</strong></p>`;
+document.querySelector("date").innerHTML = `<p><strong>${dayName} ${dateNum} ${monthName} ${year}</strong></p>`;
 
-const banner = document.querySelector('#banner');
-weekday = d.getDay();
-if (weekday == 0 || weekday == 3 || weekday == 4 || weekday == 5 || weekday == 6){
-    banner.classList.toggle("closed");
-};
+//reminder on Wednesday
+if (today.getDay() == 4) {
+  document.getElementById("banner").style.display = "block";
+}
 
