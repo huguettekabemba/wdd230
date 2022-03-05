85297113e34a91e10eb2dc21de172d03550afd67
huguettekabemba <kab21012@byui.edu>
2022-03-05 05:18:00 +0200

    comm

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -60,3 +60,45 @@ document.querySelector("date").innerHTML = `<p><strong>${dayName} ${dateNum} ${m
 if (today.getDay() == 4) {
   document.getElementById("banner").style.display = "block";
 }
+
+// Lazy Loading, gallery discover page
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
+// Number of visits
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit!`;
+}
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+
+  
