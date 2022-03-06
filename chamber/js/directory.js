e89bb780dcb67f17eeb8b0a8bcf0326e7020d289
huguettekabemba <kab21012@byui.edu>
2022-03-06 04:08:46 +0200

    comm

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,117 +1,68 @@
-const cardList = document.querySelector(".card-list");
-const directoryURL = "https://github.com/huguettekabemba/wdd230/blob/main/chamber/data/data.json";
-const medium = window.matchMedia("(min-width:545px) and (max-width: 1079px)");
+const requestURL = 'https://github.com/huguettekabemba/wdd230/blob/main/chamber/data/data.json';
+const cards = document.querySelector('#cards');
+const list = document.querySelector('#list');
 
-let width = window.innerWidth;
-
-function make_cards(business) {
-    //Create cards from each item of the fetched list
-  return `<div class="card">
-            <img src = ${business.imageurl} alt = ${business.name}>
-            <address>${business.street}, ${business.city}</address>
-            <p>${business.number}</p>
-            <a href="${business.website}" target="_blank">${business.website}</a>
-        </div>`;
-}
-
-function make_list(business) {
-    //Create list items from each item of the fetched list
-
-  return `
-    <tr>
-    <td class="name">${business.name}</td>
-    <td class="list-address">${business.street},<br>${business.city}</td>
-    <td class="list-number">${business.number}</td>
-    <td><a href="${business.website}" target="_blank">${business.website}</a></td>
-    </tr>`;
-}
-
-class setDefView {
-    // initialzes with a def view then the method setView checks to set a default view on resize but prevent the chrome mobile
-    // scroll resize from triggering the resize event
-
-    constructor(medium, list){
-        this.list = list;
-
-        if (medium.matches) {
-            // If media query matches
-            listView(this.list);
-        } else {
-            gridView(this.list);
-        }
-
-    }
-
-    setView(medium) {
-        //checks the viewport and set the defailt view either grid or list
-        if(window.innerWidth != width){
-            if (medium.matches) {
-                // If media query matches
-                listView(this.list);
-            } else {
-                gridView(this.list);
-            }
-        }
-
-    }
-}
-
-// Fetch the JSON data for the directory
-fetch(directoryURL)
-  .then((response) => {
-    return response.ok ? response.json() : console.log("error");
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
   })
-  .then((data) => {
-    console.table(data);
-    let businesses = data["businesses"];
-
-    //A calls the funtion that sets the default view for medium screen as list ans leaves the rest as cards
-    const defView = new setDefView(medium, businesses);
-    defView.setView(medium);
-    window.addEventListener("resize", () => defView.setView(medium));
-
-    //event listener for button click
-    document
-      .querySelector("#grid-view")
-      .addEventListener("click", () => gridView(businesses));
-    document
-      .querySelector("#list-view")
-      .addEventListener("click", () => listView(businesses));
+  .then(function (jsonObject) {
+      const companies = jsonObject['companies'];
+      companies.forEach(displayCards);
+      companies.forEach(displayList);
   });
 
-function gridView(business) {
-// change button color to currently active
-  let gridBtn = document.querySelector("#grid-view");
-  let listBtn = document.querySelector("#list-view");
+function displayCards(company) {
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let site = document.createElement('p');
 
-  gridBtn.style.backgroundColor = "white";
-  listBtn.style.backgroundColor = "#A9D6E5";
+    logo.setAttribute('src', company.logourl);
+    logo.setAttribute('class', 'company_logo')
 
-  //sends the grid view to the html page
-  cardList.innerHTML = "";
-  const card = business.map(make_cards);
-  const cards = document.createElement("section");
-  cards.innerHTML = card.join(" ");
-  cards.className = "cards";
-  cardList.appendChild(cards);
+    address.textContent = company.address;
+    phone.textContent = company.phone;
+    site.textContent = company.site;
+    
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(site);
+
+    cards.appendChild(card);
 }
 
-function listView(business) {
-
-    // change button color to currently active
-  let gridBtn = document.querySelector("#grid-view");
-  let listBtn = document.querySelector("#list-view");
-
-  gridBtn.style.backgroundColor = "#A9D6E5";
-  listBtn.style.backgroundColor = "white";
-    //sends the list view to the html page
-
-  cardList.innerHTML = "";
-
-  const list = business.map(make_list);
-  const items = document.createElement("tbody");
-  items.innerHTML = list.join(" ");
-  items.className = "list";
-  cardList.appendChild(items);
+function displayList(company) {
+    let item = document.createElement('li');
+    let site = document.createElement('a');
+    item.textContent = `${company.name} | ${company.address} | ${company.phone} | `;
+    site.setAttribute('href', company.site);
+    site.textContent = company.site;
+    item.appendChild(site);
+    list.appendChild(item)
 }
 
+const button = document.querySelector('#view');
+button.onclick= swapView;
+
+function swapView() {
+    console.log('triggered')
+    if (document.querySelector('#view').classList.contains('listview')) {
+        document.querySelector('#cards').classList.add('cards');
+        document.querySelector('#cards').classList.remove('null');
+        document.querySelector('#list').classList.add('null');
+        document.querySelector('#list').classList.remove('list');
+        document.querySelector('#view').classList.add('cardview');
+        document.querySelector('#view').classList.remove('listview');
+    }
+    else {
+        document.querySelector('#list').classList.add('list');
+        document.querySelector('#list').classList.remove('null');
+        document.querySelector('#cards').classList.add('null');
+        document.querySelector('#cards').classList.remove('cards');
+        document.querySelector('#view').classList.add('listview');
+        document.querySelector('#view').classList.remove('cardview');
+    }
+}
