4429645e9e6b937daa217475990bdb46f6fb0c8b
huguettekabemba <kab21012@byui.edu>
2022-03-09 02:55:57 +0200

    comm

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -22,42 +22,23 @@ x.onclick = toggleMenu;
 
 
 //day names array
-const daynames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+var currentDate = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-// month name array
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+document.querySelector(".date").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();
 
-const today = new Date();
-const dayName = daynames[today.getDay()];
-const monthName = months[today.getMonth()];
-const dateNum = today.getDate();
-const year = today.getFullYear();
-
-document.querySelector("date").innerHTML = `<p><strong>${dayName} ${dateNum} ${monthName} ${year}</strong></p>`;
 
 //reminder on Wednesday
 if (today.getDay() == 4) {
   document.getElementById("banner").style.display = "block";
 }
 
+// number of visits
+function pagedays() {
+  var date2 = d.getTime()
+  var date1 = JSON.parse(window.localStorage.getItem('date1'))
+  var timeDifference = parseInt((date2 - date1)/(1000 * 3600 * 24));
+  window.localStorage.setItem('date1', JSON.stringify(date2));
+  document.querySelector('#days').innerHTML = 'Days Since Last Visit: ' + timeDifference;
+}
