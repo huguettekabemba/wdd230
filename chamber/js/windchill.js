814c7fa1ac3d498dfe62eb3b368cccc3ea6556e6
huguettekabemba <kab21012@byui.edu>
2022-03-12 04:15:10 +0200

    commitw

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -20,7 +20,7 @@ windchill();
 
 
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=johannesburg&units=imperial&appid=3f80ad643b1ab0f399aea373e0e5e884";
+  "https://api.openweathermap.org/data/2.5/weather?q=johannesburg&units=metric&appid=3f80ad643b1ab0f399aea373e0e5e884";
 
 fetch(apiURL)
   .then((response) => response.json())
