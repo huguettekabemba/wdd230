6f290a2e1c0c15983e16b9fec053b2f147ac4085
huguettekabemba <kab21012@byui.edu>
2022-04-07 07:11:57 +0200

    temple

---
+++ final project/services/mission/scripts/finalScripts.js
@@ -0,0 +1,66 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+} 
+
+function fullYear() {
+    var y = new Date() 
+    document.write(y.getFullYear()) 
+}
+
+//script from gallery
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("thumbnail");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+//script from quotes (testimonials) slider
+  var slideIndex = 1;
+  showQuoteSlides(slideIndex);
+  
+  function plusQuoteSlides(n) {
+  showQuoteSlides(slideIndex += n);
+  }
+  
+  function currentQuoteSlide(n) {
+  showQuoteSlides(slideIndex = n);
+  }
+  
+  function showQuoteSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("quoteSlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  }
